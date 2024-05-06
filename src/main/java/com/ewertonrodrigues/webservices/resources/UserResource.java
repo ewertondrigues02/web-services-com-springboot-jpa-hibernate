2aa0ca80a0ed4c0eaf91a957dddc45af7f89aefa
ewertondrigues02 <ewertonrodrigues01@gmail.com>
2024-05-06 06:16:18 -0300

    Criação das classes Entity e EntityResource

---
+++ src/main/java/com/ewertonrodrigues/webservices/resources/UserResource.java
@@ -0,0 +1,27 @@
+package com.ewertonrodrigues.webservices.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ewertonrodrigues.webservices.entities.User;
+
+@RestController
+@RequestMapping(value= "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "456789455", "123456");
+		return ResponseEntity.ok().body(u);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
