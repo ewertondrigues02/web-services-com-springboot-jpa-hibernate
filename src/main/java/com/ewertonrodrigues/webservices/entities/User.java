09850d2707cf725ec01b9ff26a9440c255881c68
ewertondrigues02 <ewertonrodrigues01@gmail.com>
2024-05-07 07:19:11 -0300

    Entity Order, Intant, ISO 8601

--- src/main/java/com/ewertonrodrigues/webservices/entities/User.java
+++ src/main/java/com/ewertonrodrigues/webservices/entities/User.java
@@ -1,11 +1,14 @@
 package com.ewertonrodrigues.webservices.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 
@@ -74,6 +80,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
