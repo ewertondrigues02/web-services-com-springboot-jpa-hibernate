f9513904aec8fca047cdc584b31449bcfbcb082e
ewertondrigues02 <ewertonrodrigues01@gmail.com>
2024-05-08 09:04:36 -0300

    Many-TO-Many association with Join Table

--- src/main/java/com/ewertonrodrigues/webservices/config/TestConfig.java
+++ src/main/java/com/ewertonrodrigues/webservices/config/TestConfig.java
@@ -53,6 +53,15 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		User u1 = new User(null, "Linux Tovald", "linux@gmail.com", "21365478", "123456");
 		User u2 = new User(null, "Bill Gates", "william@gmail.com", "41253698", "111009");
 
