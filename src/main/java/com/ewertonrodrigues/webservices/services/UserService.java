a1fd0f44a7eaca7e3e8c963751b3294768f8ba00
ewertondrigues02 <ewertonrodrigues01@gmail.com>
2024-05-09 10:39:06 -0300

    Exception Handling findById

--- src/main/java/com/ewertonrodrigues/webservices/services/UserService.java
+++ src/main/java/com/ewertonrodrigues/webservices/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.ewertonrodrigues.webservices.entities.User;
 import com.ewertonrodrigues.webservices.repositories.UserRepository;
+import com.ewertonrodrigues.webservices.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
@@ -32,7 +33,7 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
@@ -43,6 +44,6 @@ public class UserService {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 }
