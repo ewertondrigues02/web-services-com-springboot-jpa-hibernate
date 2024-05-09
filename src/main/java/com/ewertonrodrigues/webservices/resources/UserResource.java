e71eb1f77aee56734d562ef11925acc9cadbf6d1
ewertondrigues02 <ewertonrodrigues01@gmail.com>
2024-05-09 09:26:13 -0300

    Delete User

--- src/main/java/com/ewertonrodrigues/webservices/resources/UserResource.java
+++ src/main/java/com/ewertonrodrigues/webservices/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.ewertonrodrigues.webservices.entities.User;
 import com.ewertonrodrigues.webservices.services.UserService;
 
+/**
+ * 
+ */
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
@@ -42,4 +46,19 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
+	
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
+	
+	
 }
+
+
+
+
